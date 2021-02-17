9471d97c3a7f51504f2cbe4fc843d1d840acd7e3
pamnana <nupam2543@gmail.com>
2021-02-17 19:34:20 +0700

    Beginning bootstrap for note

--- spacex-w/src/App.js
+++ spacex-w/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button>Test Button</Button>
+        
       </header>
     </div>
   );
