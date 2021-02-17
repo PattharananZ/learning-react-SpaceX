9c89bc921872d0164628b1a2ddd054d05f6f69e8
pamnana <nupam2543@gmail.com>
2021-02-17 21:42:57 +0700

    up image and home (menu)

--- spacex-w/src/App.js
+++ spacex-w/src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
+import logo1 from './image/logoblack.png';
 import './App.css';
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Breadcrumb, Image, Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="App-logo">
+          <Image src={logo1} className="img-logo"/>
+        </div>
+        <div className="menu">
+          <Breadcrumb>
+            <Breadcrumb.Item active>Home</Breadcrumb.Item>
+            <Breadcrumb.Item href="">Rocket</Breadcrumb.Item>
+            <Breadcrumb.Item href="">Launches</Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
       </header>
+      <Container>
+        
+      </Container>
     </div>
+
   );
 }
 
