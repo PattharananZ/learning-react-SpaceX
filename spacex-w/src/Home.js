67746e10f2dd3ebb3a9328a9a907d90afc1a3054
pamnana <nupam2543@gmail.com>
2021-02-18 04:32:34 +0700

    home (not complete)

--- spacex-w/src/Home.js
+++ spacex-w/src/Home.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Badge } from 'react-bootstrap';
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Button } from 'react-bootstrap';
+import logo2 from './image/logowhite.png';
+
 const Rocket = () => {
-    return(
-        <div>Just do it</div>
+    return (
+        <Container fluid>
+            <div className="content page1">
+                <img src={logo2} className="img1" />
+                <span className="text-1">SpaceX designs, manufactures and launches advanced rockets and spacecraft. The company was founded in 2002 to revolutionize space technology, with the ultimate goal of enabling people to live on other planets.</span>
+                <Button variant="outline-dark">Learn More</Button>
+                <div class="box">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div className="content page2">
+
+            </div>
+            <div className="content page3">
+
+            </div>
+            <div className="content page4">
+
+            </div>
+        </Container>
     )
 }
 export default Rocket
